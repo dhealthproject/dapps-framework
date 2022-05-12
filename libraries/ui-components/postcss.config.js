0a692e6dc43edfad0be816d3f1e11aa98757cfa9
Gregory Saive <greg@evias.be>
2022-05-12 15:04:50 +0200

    [@dhealth/components] chore(build): Adds SCSS build, TailWind and PostCSS commands.

---
+++ libraries/ui-components/postcss.config.js
@@ -0,0 +1,22 @@
+/**
+ * This file is part of dHealth dApps Framework shared under LGPL-3.0
+ * Copyright (C) 2022-present dHealth Network, All rights reserved.
+ *
+ * @package     dHealth dApps Framework
+ * @subpackage  Config
+ * @author      dHealth Network <devs@dhealth.foundation>
+ * @license     LGPL-3.0
+ */
+module.exports = () => ({
+  map: {
+    inline: false,
+    annotation: true,
+    sourcesContent: true,
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {
+      cascade: false,
+    },
+  },
+});
