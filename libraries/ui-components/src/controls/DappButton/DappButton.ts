796f3a87e7a6e7423a78dc577eef547714d22d52
Son Nguyen <n.anhson89@gmail.com>
2022-06-08 13:44:52 +0700

    [@dhealth/components] feat(controls): Update @click handler for DappButton

--- libraries/ui-components/src/controls/DappButton/DappButton.ts
+++ libraries/ui-components/src/controls/DappButton/DappButton.ts
@@ -62,53 +62,20 @@ class ComponentProperties {
  */
 export default class DappButton extends Vue.with(ComponentProperties) {
   /**
-   * The router instance that is stored in this class when the host application registers routes.
-   *
-   * This instance will be available upon this call in host application:
-   * ```js
-   * app.use(router)
-   * ```
-   * @see https://router.vuejs.org/guide/#javascript
-   * @var {any}
-   */
-  $router: any;
-
-  /**
    * Method to generically handle `@click` event of this component.
    *
    * This generally will handle either:
    *  1. A href (route/url)
    *  2. A closure (onClick)
    *
-   * This method detects whether the passed-in `href` prop is a route or an external url.
-   *  - If it is an external url, apply `window.open()`.
-   *  - If it is a route, apply `$router.push()`.
-   *  - In case `href` is not registered as a prop, a general `this.$emit("click")`.
+   * This method handle button onClick event based on the availability of the `href` prop.
+   *  - If href is available, assign `location.href` to it.
+   *  - In case `href` is not registered as a prop, call `this.$emit("click")`.
    *
    * @access public
    * @returns {void}
    */
   public handleClick(): void {
-    if (!this.href) {
-      this.$emit("click");
-    } else if (this.routeExists(this.href)) {
-      this.$router?.push(this.href);
-    } else {
-      window.open(this.href);
-    }
-  }
-
-  /**
-   * Method to check whether a route path exists within the host application.
-   * It traverses through all the existing route paths and find a match with the input.
-   *
-   * @access private
-   * @param   {string} routePath  The route path to check
-   * @returns {boolean}
-   */
-  private routeExists(routePath: string): boolean {
-    return this.$router
-      .getRoutes()
-      .some((routeObj: { path: string }) => routePath === routeObj.path);
+    this.href ? (location.href = this.href) : this.$emit("click");
   }
 }
