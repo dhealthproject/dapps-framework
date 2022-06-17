3bcd196919034c82d0adc2b53327ac13b5c1b4b7
Kravchenko <vitaliy@dhealth.foundation>
2022-06-17 17:40:01 +0300

    [@dhealthdapps/frontend] feat(Onboarding): removed createTransactionRequest to .ts file

--- runtime/dapp-frontend-vue/src/views/OnboardingPage/OnboardingPage.ts
+++ runtime/dapp-frontend-vue/src/views/OnboardingPage/OnboardingPage.ts
@@ -15,22 +15,23 @@ import { MetaView } from "@/views/MetaView";
 
 import Header from "@/components/Header/Header.vue";
 import Footer from "@/components/Footer/Footer.vue";
-import { DappTokenAmount } from "@dhealth/components";
+import { DappQR } from "@dhealth/components";
 import {
   TransferTransaction,
   Deadline,
   Address,
   PlainMessage,
   NetworkType,
+  Transaction,
 } from "@dhealth/sdk";
 
-import QRCodeGenerator from "@dhealth/qr-library";
+import { QRCodeGenerator } from "@dhealth/qr-library";
 
 @Component({
   components: {
     Header,
     Footer,
-    DappTokenAmount,
+    DappQR,
   },
 })
 export default class OnboardingPage extends MetaView {
@@ -43,16 +44,39 @@ export default class OnboardingPage extends MetaView {
     ];
   }
 
-  get getQR() {
-    return QRCodeGenerator;
+  // get getQR() {
+  //   return QRCodeGenerator;
+  // }
+
+  // get networkType() {
+  //   return NetworkType.MAIN_NET;
+  // }
+
+  // protected createLoginContract() {
+  //   return TransferTransaction.create(
+  //     Deadline.create(1616978397),
+  //     Address.createFromRawAddress("NDEVUP43ATEX2BM6XDFKVELVGQF66HOTZTIMJ6I"),
+  //     [],
+  //     PlainMessage.create("I am leaving dHealth Tech Chat #4"),
+  //     NetworkType.MAIN_NET
+  //   );
+  // }
+
+  protected createLoginContract(): any {
+    console.log(this.getTransactionRequest());
+    return QRCodeGenerator.createTransactionRequest(
+      this.getTransactionRequest(),
+      NetworkType.MAIN_NET,
+      "ED5761EA890A096C50D3F50B7C2F0CCB4B84AFC9EA870F381E84DDE36D04EF16"
+    );
   }
 
-  protected createLoginContract() {
+  protected getTransactionRequest(): Transaction {
     return TransferTransaction.create(
       Deadline.create(1616978397),
       Address.createFromRawAddress("NDEVUP43ATEX2BM6XDFKVELVGQF66HOTZTIMJ6I"),
       [],
-      PlainMessage.create("I am joining dHealth Tech Chat #4"),
+      PlainMessage.create("I am leaving dHealth Tech Chat #4"),
       NetworkType.MAIN_NET
     );
   }
