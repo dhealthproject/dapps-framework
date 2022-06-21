9fdb8deca6b7c359d786a19e1802e928007aaf00
Kravchenko <vitaliy@dhealth.foundation>
2022-06-21 18:30:13 +0300

    [@dhealthdapps/frontend] feat(Onboarding): added tests(WIP)

---
+++ runtime/dapp-frontend-vue/tests/unit/OnboardingPage/Header.spec.ts
@@ -0,0 +1,41 @@
+/**
+ * This file is part of dHealth dApps Framework shared under LGPL-3.0
+ * Copyright (C) 2022-present dHealth Network, All rights reserved.
+ *
+ * @package     dHealth UI Components
+ * @subpackage  Unit Tests
+ * @author      dHealth Network <devs@dhealth.foundation>
+ * @license     LGPL-3.0
+ */
+import { expect } from "chai";
+import { mount, Wrapper, createLocalVue } from "@vue/test-utils";
+import Header from "@/components/Header/Header.vue";
+
+// creates local vue instance for tests
+const localVue = createLocalVue();
+const componentOptions = {
+  localVue,
+  propsData: {
+    links: [
+      { path: "#", text: "Home", icon: "" },
+      { path: "#1", text: "Fitness", icon: "" },
+      { path: "#2", text: "Mindfulness", icon: "" },
+      { path: "#3", text: "Wellness", icon: "" },
+    ],
+  },
+};
+
+describe("Header -->", () => {
+  let widget: Wrapper<Vue>;
+  beforeEach(() => {
+    widget = mount(Header as any, componentOptions);
+  });
+
+  it("should display header", () => {
+    expect(widget.find("div").classes()).to.include("header");
+  });
+
+  it("should have header", () => {
+    expect(true);
+  });
+});
