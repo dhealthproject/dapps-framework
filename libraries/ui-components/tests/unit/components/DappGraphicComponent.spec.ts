811edaf44b4bfcfd31359473a82bae3f557441d4
Son Nguyen <n.anhson89@gmail.com>
2022-07-09 01:21:37 +0700

    [@dhealth/components] feat(graphics): Create DappGraphicComponent, DappMessageCircle, DappMosaicCircle, DappTransactionArrow as graphic components

---
+++ libraries/ui-components/tests/unit/components/DappGraphicComponent.spec.ts
@@ -0,0 +1,42 @@
+/**
+ * This file is part of dHealth dApps Framework shared under LGPL-3.0
+ * Copyright (C) 2022-present dHealth Network, All rights reserved.
+ *
+ * @package     dHealth UI Components
+ * @subpackage  Unit Tests
+ * @author      dHealth Network <devs@dhealth.foundation>
+ * @license     LGPL-3.0
+ */
+import { expect } from "chai";
+import { createLocalVue, shallowMount, Wrapper } from "@vue/test-utils";
+import DappGraphicComponent from "@/graphics/DappGraphicComponent/DappGraphicComponent.vue";
+import flushPromises from "flush-promises";
+
+// creates local vue instance for tests
+const localVue = createLocalVue();
+const componentOptions = {
+  localVue,
+};
+
+describe("DappGraphicComponent -->", () => {
+  let widget: Wrapper<Vue>;
+  beforeEach(() => {
+    widget = shallowMount(DappGraphicComponent as any, componentOptions);
+  });
+
+  it("should have correct default props", () => {
+    expect(widget.props().x).to.equals(0);
+    expect(widget.props().y).to.equals(0);
+  });
+
+  it("should have correct inputed props", async () => {
+    const propsData = {
+      x: 100,
+      y: 200,
+    };
+    widget.setProps(propsData);
+    await flushPromises();
+    expect(widget.props().x).to.equals(100);
+    expect(widget.props().y).to.equals(200);
+  });
+});
