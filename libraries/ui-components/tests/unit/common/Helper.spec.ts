1ca55aaea0f316dc4a4dca837a81d9b199e194c3
Son Nguyen <n.anhson89@gmail.com>
2022-08-15 23:13:43 +0700

    [@dhealth/components] fix(build): apply linter with last changes

--- libraries/ui-components/tests/unit/common/Helper.spec.ts
+++ libraries/ui-components/tests/unit/common/Helper.spec.ts
@@ -9,12 +9,12 @@
  */
 // external dependencies
 import { expect } from "chai";
-import sinon from 'sinon';
+import sinon from "sinon";
 
 // internal dependencies
-import { Helper } from '@/common/Helper';
+import { Helper } from "@/common/Helper";
 
-describe('Helper -->', () => {
+describe("Helper -->", () => {
   const sandbox = sinon.createSandbox();
   let consoleErrorStub: sinon.SinonStub;
 
@@ -23,15 +23,17 @@ describe('Helper -->', () => {
   });
 
   afterEach(() => {
-    sandbox.restore()
+    sandbox.restore();
   });
 
-  describe('test on getColorFromHash()', async () => {
+  describe("test on getColorFromHash()", async () => {
     it("should print error to console and return correct result when hash contains less than 3 characters", () => {
       const hashParam = "ab";
       const result = Helper.getColorFromHash(hashParam);
       expect(
-        consoleErrorStub.calledWith("Failed to convert hash to color. Hash string length < 3")
+        consoleErrorStub.calledWith(
+          "Failed to convert hash to color. Hash string length < 3"
+        )
       ).to.be.true;
       expect(result).to.deep.equal({
         R: 0,
@@ -42,47 +44,56 @@ describe('Helper -->', () => {
 
     it("should have correct flow & result when isHex = true", () => {
       const hashParam = "abcdefghi";
-      const mathTruncStub = sandbox
-        .stub(Math, "trunc")
-        .returns(3);
+      const mathTruncStub = sandbox.stub(Math, "trunc").returns(3);
       const substringStub = sandbox
         .stub(String.prototype, "substring")
-        .onCall(0).returns("abc")
-        .onCall(1).returns("def")
-        .onCall(2).returns("ghi");
+        .onCall(0)
+        .returns("abc")
+        .onCall(1)
+        .returns("def")
+        .onCall(2)
+        .returns("ghi");
       const hexToRGBStub = sandbox
         .stub(Helper, "hexToRGB")
-        .onCall(0).returns(0)
-        .onCall(1).returns(1)
-        .onCall(2).returns(2);
+        .onCall(0)
+        .returns(0)
+        .onCall(1)
+        .returns(1)
+        .onCall(2)
+        .returns(2);
       const result = Helper.getColorFromHash(hashParam, true);
       expect(mathTruncStub.calledOnce).to.be.true;
       expect(substringStub.calledThrice).to.be.true;
       expect(hexToRGBStub.calledThrice).to.be.true;
-      expect(result).to.deep.equal({R: 0, G: 1, B: 2});
+      expect(result).to.deep.equal({ R: 0, G: 1, B: 2 });
     });
 
     it("should have correct flow & result when isHex = false", () => {
       const hashParam = "abcdefghi";
-      const mathTruncStub = sandbox
-        .stub(Math, "trunc")
-        .returns(3);
+      const mathTruncStub = sandbox.stub(Math, "trunc").returns(3);
       const substringStub = sandbox
         .stub(String.prototype, "substring")
-        .onCall(0).returns("abc")
-        .onCall(1).returns("def")
-        .onCall(2).returns("ghi")
-        .onCall(3).returns("cde");
+        .onCall(0)
+        .returns("abc")
+        .onCall(1)
+        .returns("def")
+        .onCall(2)
+        .returns("ghi")
+        .onCall(3)
+        .returns("cde");
       const charsetToRGBStub = sandbox
         .stub(Helper, "charsetToRGB")
-        .onCall(0).returns(0)
-        .onCall(1).returns(1)
-        .onCall(2).returns(2);
+        .onCall(0)
+        .returns(0)
+        .onCall(1)
+        .returns(1)
+        .onCall(2)
+        .returns(2);
       const result = Helper.getColorFromHash(hashParam, false);
       expect(mathTruncStub.calledOnce).to.be.true;
       expect(substringStub.callCount === 4).to.be.true;
       expect(charsetToRGBStub.calledThrice).to.be.true;
-      expect(result).to.deep.equal({R: 0, G: 1, B: 2});
+      expect(result).to.deep.equal({ R: 0, G: 1, B: 2 });
     });
   });
 
@@ -90,9 +101,12 @@ describe('Helper -->', () => {
     it("should have correct flow and result", () => {
       const parseIntStub = sandbox
         .stub(global, "parseInt")
-        .onFirstCall().returns(1)
-        .onSecondCall().returns(2)
-        .onThirdCall().returns(3);
+        .onFirstCall()
+        .returns(1)
+        .onSecondCall()
+        .returns(2)
+        .onThirdCall()
+        .returns(3);
       const expectedResult = 34;
       const result = Helper.hexToRGB("abc");
       expect(parseIntStub.callCount === 3).to.be.true;
