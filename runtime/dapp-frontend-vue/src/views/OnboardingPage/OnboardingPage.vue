8c240200817a6684eb93e4d972ecc424032bdf7c
kravchenkodhealth <vitaliy@dhealth.foundation>
2022-08-22 15:26:06 +0300

    [@dhealthdapps/frontend] fix(style): background color of qr and dappbutton on hover, alignment of button in dappqr

---
+++ runtime/dapp-frontend-vue/src/views/OnboardingPage/OnboardingPage.vue
@@ -0,0 +1,32 @@
+<!--
+/**
+ * This file is part of dHealth dApps Framework shared under LGPL-3.0
+ * Copyright (C) 2022-present dHealth Network, All rights reserved.
+ *
+ * @package     dHealth dApps Framework
+ * @subpackage  Vue Frontend
+ * @author      dHealth Network <devs@dhealth.foundation>
+ * @license     LGPL-3.0
+ */
+-->
+
+<template>
+  <div class="dapp-screen-onboarding">
+    <Header :links="dummyLinks" />
+    <Loader v-if="isLoading" />
+    <DappQR v-else :qrCode="createLoginContract()" />
+    <Footer :links="dummyLinks" />
+  </div>
+</template>
+
+<script lang="ts" src="./OnboardingPage.ts"></script>
+
+<style lang="scss" scoped>
+// temporary fix for QR and DappButton bgcolor
+.style-primary {
+  background-color: transparent !important;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+</style>
