38351d9bca73a6f4b80f8c792ede8fe10e46b2bc
Son Nguyen <n.anhson89@gmail.com>
2022-11-04 23:15:05 +0700

    [@dhealthdapps/backend] test: add unit tests for notifier scope

---
+++ runtime/backend/tests/unit/notifier/concerns/NotifierFactory.spec.ts
@@ -0,0 +1,57 @@
+/**
+ * This file is part of dHealth dApps Framework shared under LGPL-3.0
+ * Copyright (C) 2022-present dHealth Network, All rights reserved.
+ *
+ * @package     dHealth dApps Framework
+ * @subpackage  Backend
+ * @author      dHealth Network <devs@dhealth.foundation>
+ * @license     LGPL-3.0
+ */
+// external dependencies
+import { MailerService } from "@nestjs-modules/mailer";
+import { Test, TestingModule } from "@nestjs/testing";
+
+// internal dependencies
+import { NotifierFactory } from "../../../../src/notifier/concerns/NotifierFactory";
+import { EmailNotifier } from "../../../../src/notifier/services/EmailNotifier";
+import { NotifierType } from "../../../../src/notifier/models/NotifierType";
+
+
+describe("notifier/NotifierFactory", () => {
+  let service: NotifierFactory;
+  let emailNotifier: EmailNotifier;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        NotifierFactory,
+        EmailNotifier,
+        {
+          provide: MailerService,
+          useValue: MailerService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<NotifierFactory>(NotifierFactory);
+    emailNotifier = module.get<EmailNotifier>(EmailNotifier);
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  describe("getNotifier()", () => {
+    it("should return correct instance of Notifier", () => {
+      // prepare
+      const expectedResults = [ null, emailNotifier ];
+      [null, NotifierType.MAIL].forEach((transport, index) => {
+        // act
+        const result = service.getNotifier(transport);
+
+        // assert
+        expect(result).toBe(expectedResults[index]);
+      });
+    });
+  });
+});
