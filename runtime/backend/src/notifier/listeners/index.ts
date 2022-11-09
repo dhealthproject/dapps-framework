9f3523689bbb3185a055890ebb1b361d499ee1d7
Son Nguyen <n.anhson89@gmail.com>
2022-11-09 22:34:20 +0700

    [@dhealthdapps/backend] docs(notifier): add index files in notifier scope

---
+++ runtime/backend/src/notifier/listeners/index.ts
@@ -0,0 +1,11 @@
+/**
+ * This file is part of dHealth dApps Framework shared under LGPL-3.0
+ * Copyright (C) 2022-present dHealth Network, All rights reserved.
+ *
+ * @package     dHealth dApps Framework
+ * @subpackage  Backend
+ * @author      dHealth Network <devs@dhealth.foundation>
+ * @license     LGPL-3.0
+ */
+// exported listeners
+export * from "./AlertNotifier";
