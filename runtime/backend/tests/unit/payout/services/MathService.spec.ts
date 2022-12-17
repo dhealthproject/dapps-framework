5cc7d9772a7b96d56102f702d5e01195b5b8ee68
Son Nguyen <n.anhson89@gmail.com>
2022-12-17 23:04:05 +0700

    [@dhealthdapps/backend] test: update unit tests

--- runtime/backend/tests/unit/payout/services/MathService.spec.ts
+++ runtime/backend/tests/unit/payout/services/MathService.spec.ts
@@ -16,22 +16,30 @@ describe("payout/MathService", () => {
 
   beforeEach(() => {
     mathService = new MathService();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 
   describe("skewNormal()", () => {
     it("should return correct result if skewness is not defined", () => {
-      // prepare
-      const [mean, deviation] = [1, 2];
-      const getRandomVariatesCall = jest
-        .spyOn((mathService as any), "getRandomVariates")
-        .mockReturnValue([1,1]);
+      const expectedResullts = [3, -4];
+      [1, -2.5].forEach((u0: number, index: number) => {
+        // prepare
+        jest.clearAllMocks();
+        const [mean, deviation] = [1, 2];
+        const getRandomVariatesCall = jest
+          .spyOn((mathService as any), "getRandomVariates")
+          .mockReturnValue([u0, 1]);
 
-      // act
-      const result = mathService.skewNormal(mean, deviation);
+        // act
+        const result = mathService.skewNormal(mean, deviation);
 
-      // assert
-      expect(getRandomVariatesCall).toHaveBeenCalledTimes(1);
-      expect(result).toBe(3);
+        // assert
+        expect(getRandomVariatesCall).toHaveBeenCalledTimes(1);
+        expect(result).toBe(expectedResullts[index]);
+      });
     });
 
     it("should return correct result if skewness is defined", () => {
@@ -53,6 +61,31 @@ describe("payout/MathService", () => {
       expect(mathSqrtCall).toHaveBeenNthCalledWith(1, 1 + skewness * skewness);
       expect(mathSqrtCall).toHaveBeenNthCalledWith(2, 0);
       expect(result).toBe(7);
+    });
+
+    it("should return correct result if first random variate number is negative", () => {
+      const expectedResullts = [0, 3];
+      [-1, -2].forEach((u0: number, index: number) => {
+        // prepare
+        jest.clearAllMocks();
+        const [mean, deviation, skewness] = [1, 2, 3];
+        const getRandomVariatesCall = jest
+          .spyOn((mathService as any), "getRandomVariates")
+          .mockReturnValue([u0, 1]);
+        const mathSqrtCall = jest
+          .spyOn(Math, "sqrt")
+          .mockReturnValue(2);
+
+        // act
+        const result = mathService.skewNormal(mean, deviation, skewness);
+
+        // assert
+        expect(getRandomVariatesCall).toHaveBeenCalledTimes(1);
+        expect(mathSqrtCall).toHaveBeenCalledTimes(2);
+        expect(mathSqrtCall).toHaveBeenNthCalledWith(1, 1 + skewness * skewness);
+        expect(mathSqrtCall).toHaveBeenNthCalledWith(2, 1 - (skewness / 2) * (skewness/ 2));
+        expect(result).toBe(expectedResullts[index]);
+      });
     });
   });
 });
